66a88268150f28a6b1c094593b1789b111fd61bb
Rodrigo Roldán <roldriel@gmail.com>
2025-08-25 12:41:24 -0300

    refactor: remove unused Union import from humanize

--- src/eones/humanize.py
+++ src/eones/humanize.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional, Tuple, Union, cast
+from typing import TYPE_CHECKING, Optional, Tuple, cast
 
 from eones.locales import get_messages
 
